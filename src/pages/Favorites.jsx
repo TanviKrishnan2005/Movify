7ae78305b2d3fe5d7272a58d21793cde201def14
Tanvi <rmtanvikrishnan@gmail.com>
2025-06-25 17:12:38 +0530

    final commit

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,26 @@
 import "../css/Favorites.css"
-function Favorites(){
-    return(
-    <div className="favorites-empty">
-        <h2>no fav yet</h2>
-        <p>start adding fav here</p>
-    </div>
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+function Favorites() {
+    const { favorites } = useMovieContext();
+
+    if (favorites) {
+        return (
+            <div className="favorites">
+                <h2>Your Favorites</h2>
+                <div className="movies-grid">
+                    {favorites.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div className="favorites-empty">
+            <h2>no fav yet</h2>
+            <p>start adding fav here</p>
+        </div>
     )
 };
 
